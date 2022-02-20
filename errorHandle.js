1c72e381885b01eef0b29780d396378822615808
Hazel Wu <tinazx056@gmail.com>
2022-02-21 01:54:14 +0800

    feat(api): 重寫一遍

--- errorHandle.js
+++ errorHandle.js
@@ -1,14 +1,14 @@
-function errorHandle(res, status, text) {
+const errorHandle = (res, status) => {
   const headers = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE'
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, DELETE, PATCH'
   }
 
   const statusText = {
-    400: '欄位未填寫正確或無此 todo id',
-    404: '無此路由',
+    400: '查無此id或缺少欄位',
+    404: '無對應路由',
     500: '伺服器發生錯誤'
   }
 
--- server.js
+++ server.js
@@ -1,64 +1,57 @@
 const http = require('http')
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 const errorHandle = require('./errorHandle')
 
-const todos = [
-  {
-    id: uuidv4(),
-    title: '今天要洗澡'
-  }
-]
+const todos = []
 
 const requestListener = (req, res) => {
   const headers = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE'
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET, DELETE, OPTIONS',
   }
-
   let body = ''
+
   // 接收資料
   req.on('data', (chunk) => {
     body += chunk
   })
 
-
-  if (req.url == '/todos' && req.method === 'GET') {
+  // 取得所有 Todos
+  if (req.url === '/todos' && req.method === 'GET') {
     res.writeHead(200, headers)
     res.write(JSON.stringify({
       status: 'success',
       data: todos
     }))
     res.end()
-  } else if (req.url == '/todos' && req.method == 'POST') {
+    // 新增一筆 todos
+  } else if (req.url === '/todos' && req.method === 'POST') {
     req.on('end', () => {
       try {
         const title = JSON.parse(body).title
-        if (title === undefined) {
-          errorHandle(res, 400)
-        } else {
+        if (title !== undefined) {
           const todo = {
-            title,
-            id: uuidv4()
+            id: uuidv4(),
+            title
           }
-
           todos.push(todo)
-
           res.writeHead(200, headers)
           res.write(JSON.stringify({
             status: 'success',
             data: todos
           }))
           res.end()
+        } else {
+          errorHandle(res, 400)
         }
-
       } catch (error) {
         errorHandle(res, 400)
       }
     })
-  } else if (req.url == '/todos' && req.method == 'DELETE') {
-
+    // 刪除所有 todos
+  } else if (req.url === '/todos' && req.method === 'DELETE') {
     todos.length = 0
     res.writeHead(200, headers)
     res.write(JSON.stringify({
@@ -75,25 +68,27 @@ const requestListener = (req, res) => {
       res.writeHead(200, headers)
       res.write(JSON.stringify({
         status: 'success',
-        data: todos,
+        data: todos
       }))
       res.end()
     } else {
       errorHandle(res, 400)
     }
+    // 更新單筆 todo
   } else if (req.url.startsWith('/todos') && req.method === 'PATCH') {
     req.on('end', () => {
+
       try {
         const title = JSON.parse(body).title
         const id = req.url.split('/').pop()
         const index = todos.findIndex(el => el.id === id)
 
-        if (title !== undefined && index !== -1) {
+        if (title && index !== -1) {
           todos[index].title = title
           res.writeHead(200, headers)
           res.write(JSON.stringify({
             status: 'success',
-            data: todos,
+            data: todos
           }))
           res.end()
         } else {
@@ -104,11 +99,11 @@ const requestListener = (req, res) => {
         errorHandle(res, 400)
       }
     })
+
   } else if (req.method === 'OPTIONS') {
-    res.writeHead(200, headers);
+    res.writeHead(200, headers)
     res.end()
-  }
-  else {
+  } else {
     errorHandle(res, 404)
   }
 }

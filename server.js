92647e5aac917e06240670aa05d4146482aa470a
Hazel Wu <tinazx056@gmail.com>
2022-02-20 23:23:05 +0800

    feat(api): 刪除單筆代辦 API

--- server.js
+++ server.js
@@ -66,6 +66,21 @@ const requestListener = (req, res) => {
       data: todos
     }))
     res.end()
+  } else if (req.url.startsWith('/todos') && req.method === 'DELETE') {
+    const id = req.url.split('/').pop()
+    const index = todos.findIndex(el => el.id === id)
+
+    if (index !== -1) {
+      todos.splice(index, 1)
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({
+        status: 'success',
+        data: todos,
+      }))
+      res.end()
+    } else {
+      errorHandle(res, 400)
+    }
   }
   else {
     errorHandle(res, 404)

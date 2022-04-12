ce77e6108d689ab62b674db648496b80349e5f7f
Hazel Wu <tinazx056@gmail.com>
2022-04-13 07:53:03 +0800

    feat: install dotenv, setup db

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import http from 'http';
 import app from './app.js'
 import mongoose from 'mongoose';
+import 'dotenv/config'
 const PORT = process.env.PORT || 3005
 
-mongoose.connect('mongodb://localhost:27017/hotel')
+// 資料庫
+const DB = process.env.DB_URL.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+)
+const localDB = 'mongodb://localhost:27017/hotel'
+
+mongoose.connect(localDB || DB)
   .then(() => {
     console.log('資料庫連接成功')
   })
-  .catch(error => console.log(error))
+  .catch(error => console.log('資料庫連接失敗', error))
 
 const server = http.createServer(app)
 server.listen(PORT)

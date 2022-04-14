e0c5aa2c632b6f9edc50f00abcfbc5deb0c20f48
Hazel Wu <tinazx056@gmail.com>
2022-04-15 02:01:09 +0800

    feat: mongoose Todo Model 移除 _v

--- model/todo.js
+++ model/todo.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const todoSchema = new mongoose.Schema({
   title: String,
-})
+}, { versionKey: false })
 
 const Todo = mongoose.model('Room', todoSchema)
 export default Todo
